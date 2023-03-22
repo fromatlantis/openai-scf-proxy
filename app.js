56636dbcf281a58dba4f1d0482eabd79b0abf90a
Vincent Neo <76011299@qq.com>
2023-03-22 11:15:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ app.use('/hello', (ctx, next) => {
   ctx.body = 'Hello World!';
 });
 
-app.use('/', createProxyMiddleware({
-  target: 'https://api.openai.com',
-  changeOrigin: true,
-  onProxyRes: function (proxyRes, req, res) {
-    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-  }
-}));
+// app.use('/', createProxyMiddleware({
+//   target: 'https://api.openai.com',
+//   changeOrigin: true,
+//   onProxyRes: function (proxyRes, req, res) {
+//     proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+//   }
+// }));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
