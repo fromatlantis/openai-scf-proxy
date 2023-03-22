37f7d67728beb41ff37a957497ee726b55cc21a2
Vincent Neo <76011299@qq.com>
2023-03-22 16:45:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ app.post('/message', (req, res) => {
 app.use('/', createProxyMiddleware({
   target: 'https://api.openai.com',
   changeOrigin: true,
-  onProxyReq: (proxyReq, req, res) => {
-    // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
-    console.log('openai:req')
-    proxyReq.removeHeader('x-forwarded-for');
-    proxyReq.removeHeader('x-real-ip');
-  },
+//   onProxyReq: (proxyReq, req, res) => {
+//     // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
+//     console.log('openai:req')
+//     proxyReq.removeHeader('x-forwarded-for');
+//     proxyReq.removeHeader('x-real-ip');
+//   },
   onProxyRes: function (proxyRes, req, res) {
     console.log('openai:res')
     proxyRes.headers['Access-Control-Allow-Origin'] = '*';
