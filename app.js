d80ce1d66a497df55d2686f4eedc6768fe614f58
Vincent Neo <76011299@qq.com>
2023-03-22 11:17:44 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express()
-const port = 9000
 
 app.use('/hello', (ctx, next) => {
   ctx.body = 'Hello World!';
