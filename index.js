4b04fc7895901a3626c43e6a6603eee22310857e
Vincent Neo <76011299@qq.com>
2023-03-22 14:22:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ app.post('/hello', (req, res) => {
   res.send('欢迎来到我的网站！');
 });
 
-app.use('/', createProxyMiddleware({
-  target: 'https://api.openai.com',
-  changeOrigin: true,
-  onProxyReq: (proxyReq, req, res) => {
-    // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
-    proxyReq.removeHeader('x-forwarded-for');
-    proxyReq.removeHeader('x-real-ip');
-  },
-  onProxyRes: function (proxyRes, req, res) {
-    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-  }
-}));
+// app.use('/', createProxyMiddleware({
+//   target: 'https://api.openai.com',
+//   changeOrigin: true,
+//   onProxyReq: (proxyReq, req, res) => {
+//     // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
+//     proxyReq.removeHeader('x-forwarded-for');
+//     proxyReq.removeHeader('x-real-ip');
+//   },
+//   onProxyRes: function (proxyRes, req, res) {
+//     proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+//   }
+// }));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
