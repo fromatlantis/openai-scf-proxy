b6f4f921b6cfcb482744bfa27764ea9d8465b754
VincentNeo <76011299@qq.com>
2023-04-07 23:54:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,10 @@ app.post('/v1/chat/completions', async (req, res) => {
         res.setHeader('Connection', 'keep-alive');
       openaiRes.data.on('data', (data) => {
         console.log(data)
-           res.send(data);
+           
+        for(const chunk of data) {
+           res.send(chunk);
+        }
       })
     } catch (error) {
       res.status(500).send(error.message);
