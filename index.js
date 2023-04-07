119d8f0a745e0b94f6481c9b09f5d97082ad1977
VincentNeo <76011299@qq.com>
2023-04-07 21:59:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const openaiClient = new OpenAIApi(openaiConfig);
 
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 5000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
