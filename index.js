f968e27b7e392fc64ea89f3592daa9e03def95fe
VincentNeo <76011299@qq.com>
2023-04-08 08:54:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -90,8 +90,8 @@ app.post('/v1/images/generations', async (req, res) => {
   console.log(req);
     try {
         const openaiRes = await openaiClient.createImage(req.body);
-        
-        res.send(openaiRes.data);
+        //res.send(openaiRes.data);
+        res.send(openaiRes);
     } catch (error) {
       res.status(500).send(error.message);
     }
