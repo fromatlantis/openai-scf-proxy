3e69f49e4b13c834d002c98b8726bc4581a65526
VincentNeo <76011299@qq.com>
2023-04-08 08:28:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.post('/v1/chat/completions', async (req, res) => {
       res.status(500).send(error.message);
     }
 });
-app.post('/v1/chat/completions', async (req, res) => {
+app.post('/v1/images/generations', async (req, res) => {
     try {
         const openaiRes = await openaiClient.createImage(req.body);
         res.send(openaiRes)
