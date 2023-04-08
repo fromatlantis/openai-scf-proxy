4351e7cc8c78503a3d1569713328485498dc70c3
VincentNeo <76011299@qq.com>
2023-04-08 12:33:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -45,15 +45,15 @@ app.use(bodyParser.json());
 
 app.post('/v1/chat/completions', async (req, res) => {
     try {
-      res.send('hello')
-//       const stream = await OpenAI(
-  //     "completions",
-  //     {
-  //       model: "text-davinci-003",
-  //       prompt: "Write a happy sentence.\n\n",
-  //       max_tokens: 25
-  //     }
-  //   );
+      //res.send('hello')
+      const stream = await OpenAI(
+        "completions",
+        {
+          model: "text-davinci-003",
+          prompt: "Write a happy sentence.\n\n",
+          max_tokens: 25
+        }
+      );
 
 //   stream.pipe(res);
 //       const openaiRes = await openaiClient.createChatCompletion(req.body, { responseType: 'stream' });
